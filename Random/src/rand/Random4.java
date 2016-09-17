b0dfe69fed6b72cd4e422dc551050bb7a912274e
Ravi Teja <Ravi Teja@RaviTeja-PC>
2016-09-17 10:41:32 +0530

    commit not myself

---
+++ Random/src/rand/Random4.java
@@ -0,0 +1,10 @@
+package rand;
+
+public class Random4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("-------Random4---------");
+	}
+
+}
